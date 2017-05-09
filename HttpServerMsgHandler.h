875cb64e321f0a48a3a02729c8ed67096717b5cc
YU <yruestc@gmail.com>
2017-05-09 09:10:44 +0800

    Create HttpServerMsgHandler.h

---
+++ HttpServerMsgHandler.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "MessageHandler.h"
+
+
+class HttpServerMsgHandler : public MessageHandler
+{
+public:
+	HttpServerMsgHandler(void){};
+	virtual ~HttpServerMsgHandler(void){};
+
+	int processMessage(void *arg);
+};
